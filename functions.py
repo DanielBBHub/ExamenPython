c2d6a8a7a1cb369dd84dfb5b8e2dccc723e4c7f8
Daniel Benavides Belmonte <ruxzfly@gmail.com>
2023-03-22 10:59:06 +0100

    Commit3

--- functions.py
+++ functions.py
@@ -4,15 +4,37 @@ def read_data(nombre_archivo):
     vinos = dict()
     long_fila = len(llaves)
     linea = texto.readline()
-    i = 0
+    nDato = 0
 
     while linea != "" :
         if len(datos:=linea.split(",")) == long_fila:
-            for indice in range(len(datos)):
-                vino = {llave: datos[indice] for llave in llaves }
-                #print(vino)
-            vinos.setdefault("dato" + str(i), vino)
-            i += 1
+            vino = {llaves[indice]: datos[indice] for indice in range(len(datos))}
+            vinos.setdefault("dato" + str(nDato), vino)
+            nDato += 1
         linea = texto.readline()
     
-    print(vinos)
+    return vinos
+
+
+def split(diccionario):
+    vino_blanco = dict()
+    vino_rosa = dict()
+    nDatoW = 0
+    nDatoR = 0
+
+    for llave, vinoDict in diccionario.items():
+        llaves=list(vinoDict.keys())
+        if vinoDict.get(llaves[0])  == "white":
+            print(llaves)
+            vino = {llaves[indice]: list(vinoDict.values())[indice] for indice in range(1,len(vinoDict))}
+            vino_blanco.setdefault("dato" + str(nDatoW), vino)
+            nDatoW += 1
+            print(vino_blanco.items())
+        if vinoDict.get(llaves[0])== "red": 
+            print(llaves)
+            vino = {llaves[indice]: list(vinoDict.values())[indice] for indice in range(1,len(vinoDict))}
+            vino_rosa.setdefault("dato" + str(nDatoR), vino)
+            nDatoR += 1
+            print(vino_rosa.items())
+    
+    return vino_blanco, vino_rosa
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import functions
 
-functions.read_data("ExamenPython/winequality.csv")
+#functions.read_data("ExamenPython/winequality.csv")
+
+dict_prueba = {
+    "dato1": {
+        "type": "white",
+        "pfna": 1231,
+        "ae": 448
+    },
+    "dato2": {
+        "type": "red",
+        "pfna": 111,
+        "ae": 4888
+    }
+}
+functions.split(dict_prueba)
